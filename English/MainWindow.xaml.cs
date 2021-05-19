d4035b944cf54f8fac5b01bb2e93d3b5a269b499
Alexey Bilko <bilkoalexey67@gmail.com>
2021-05-19 14:32:59 +0300

    Add project files.

---
+++ English/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace English
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<User> users;
+
+        public MainWindow()
+        {
+            users = new List<User>();
+            GetUsers();
+
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in users)
+            {
+                if(LoginTextBox.Text == item.Login && passwordBox.Password == item.Password)
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        private void GetUsers()
+        {
+            try
+            {
+                List<string> list = File.ReadAllLines("users.txt").ToList();
+                foreach (var item in list)
+                {
+                    string[] tmp = item.Split(' ');
+                    users.Add(new User(tmp[0], tmp[1], int.Parse(tmp[2]), int.Parse(tmp[3])));
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error in file!");
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ English/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace English
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public int Level { get; set; }
+        public int Progress { get; set; }
+
+        public User(string login, string password)
+        {
+            Login = login;
+            Password = password;
+        }
+
+        public User(string login, string password, int level, int progress)
+        {
+            Login = login;
+            Password = password;
+            Level = level;
+            Progress = progress;
+        }
+    }
+}
